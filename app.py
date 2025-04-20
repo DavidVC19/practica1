d444b961a0a0d3b86034987d4f2a9ceb9536803e
DavidVC19 <146501623+DavidVC19@users.noreply.github.com>
2025-04-20 01:28:46 -0500

    ojalafuncione

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import os
 app = Flask(__name__, template_folder='templates')
 
 # Configuración de la base de datos
-DB_HOST = 'dpg-cr6bdj1u0jms73bn1teg-a.oregon-postgres.render.com'
-DB_NAME = 'dbtest_h0hy'
-DB_USER = 'dbtest_h0hy_user'
-DB_PASSWORD = 'xkmD4V6rmoGNJ27uGLq1k76ynORQ8HTd'
+DB_HOST = 'dpg-d02963ruibrs73al219g-a.oregon-postgres.render.com'
+DB_NAME = 'practica1_sgxk'
+DB_USER = 'dvilchez'
+DB_PASSWORD = 'XXyegr2czdIHwLN8C31MLhESVIKNNcSy'
 
 
 def conectar_db():
